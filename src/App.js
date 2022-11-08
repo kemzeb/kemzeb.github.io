337b5f02611b0750b4938dab508569b86f5a89c7
Kemal Zebari <kemalzebra@gmail.com>
2022-11-07 19:56:10 -0800

    feat: Implement footer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Blog from "./pages/Blog";
 
 function App() {
   return (
-    <div className="container">
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/blog" element={<Blog />} />
-        </Routes>
-      </Router>
-    </div>
+    <>
+      <div className="container">
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/blog" element={<Blog />} />
+          </Routes>
+        </Router>
+      </div>
+      <Footer />
+    </>
   );
 }
 
