9190218d28a96fe8776d2ef02a89512694591aac
Kemal Zebari <kemalzebra@gmail.com>
2023-01-17 22:18:13 -0800

    feat: Implement Projects page
    Also add a piece of text to distinguish between personal and school
    projects. This snapshot also fixes a bug with the source code link
    not disappearing when there is no link provided for a project.

--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -1,13 +1,14 @@
 import "./ProjectCards.css";
 
-function ProjectCard({ name, href, desc, skills, isPinned }) {
+function ProjectCard({ name, href, desc, skills, isPinned, isAcademic }) {
+  console.log(name, isAcademic)
   return (
     <div className="project-card">
       <div className="top-container">
         <div className="name-container">
 
-          {isPinned === true && (
-            <svg className="pinned" viewBox="0 0 16 16" version="1.1" aria-hidden="true" height="1" width="1">
+          {isPinned && (
+            <svg className="pinned" viewBox="0 0 16 16" version="1.1" aria-hidden="true">
               <path
                 fillRule="evenodd"
                 d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z"
@@ -17,12 +18,17 @@ function ProjectCard({ name, href, desc, skills, isPinned }) {
           <div className="project-name">
             {name}
           </div>
+          {isAcademic && (
+            <div className="school-project">(School Project)</div>
+          )}
         </div>
-        <a href={href} target="_blank" rel="noreferrer">
-          <svg className="source-code-link" height="20" viewBox="0 0 16 16" width="20">
-            <path fill-rule="evenodd" d="M4.72 3.22a.75.75 0 011.06 1.06L2.06 8l3.72 3.72a.75.75 0 11-1.06 1.06L.47 8.53a.75.75 0 010-1.06l4.25-4.25zm6.56 0a.75.75 0 10-1.06 1.06L13.94 8l-3.72 3.72a.75.75 0 101.06 1.06l4.25-4.25a.75.75 0 000-1.06l-4.25-4.25z"></path>
-          </svg>
-        </a>
+        {href && (
+          <a href={href} target="_blank" rel="noreferrer">
+            <svg className="source-code-link" height="20" viewBox="0 0 16 16" width="20">
+              <path fillRule="evenodd" d="M4.72 3.22a.75.75 0 011.06 1.06L2.06 8l3.72 3.72a.75.75 0 11-1.06 1.06L.47 8.53a.75.75 0 010-1.06l4.25-4.25zm6.56 0a.75.75 0 10-1.06 1.06L13.94 8l-3.72 3.72a.75.75 0 101.06 1.06l4.25-4.25a.75.75 0 000-1.06l-4.25-4.25z"></path>
+            </svg>
+          </a>
+        )}
       </div>
       <div className="desc">{desc}</div>
       <div className="skills">Skills: {skills.join(", ")}</div>
@@ -41,10 +47,29 @@ function ProjectCards({ projects }) {
           desc={project.desc}
           skills={project.skills}
           isPinned={project.isPinned}
+          isAcademic={project.isAcademic}
         />
       ))}
     </div>
   );
 }
 
+function DetailedProjectCards({ detailedProjects }) {
+  return (
+    <div className="project-cards">
+      {detailedProjects.map((project, idx) => (
+        <ProjectCard
+          key={idx} // If projects adds or deletes elements, find an alternative way to create unique keys
+          name={project.name}
+          href={project.href}
+          desc={project.desc}
+          skills={project.skills}
+          isPinned={project.isPinned}
+        />
+      ))}
+    </div>
+  );
+}
+
+export { DetailedProjectCards };
 export default ProjectCards;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,5 +1,62 @@
+import "./Projects.css"
+import ProjectCards from "../components/ProjectCards";
+
 function Projects() {
-  return "Coming Soon!";
+  const featuredProjects = [
+    {
+      name: "CodeProfiler",
+      desc: "My senior project! It combines instrumentation profiling and unit testing software into one for Go programs. More info on my LinkedIn page",
+      skills: ["Go", "SDLC", "Agile", "Code Refactoring"],
+      isPinned: true,
+      isAcademic: true,
+    },
+    {
+      name: "kemzeb/planetvplanet",
+      href: "https://github.com/kemzeb/planetvplanet",
+      desc: "🌍 Web app that compares planets in our Solar System to exoplanets",
+      skills: ["Java", "JavaScript", "React", "MySQL", "Spring Boot", "Unit Testing"],
+      isPinned: true,
+    },
+    {
+      name: "kemzeb/habiter",
+      href: "https://github.com/kemzeb/habiter",
+      desc: "Quantify and keep tabs on habits within the command-line",
+      skills: ["Python", "SQLite", "JSON", "Unit Testing", "GitHub Actions"],
+      isPinned: true,
+    },
+    {
+      name: "kemzeb/portdive",
+      href: "https://github.com/kemzeb/portdive",
+      desc: "CLI game inspired by Splinter Cell: Chaos Theory's hacking mini-game",
+      skills: ["Go", "Game Development"],
+      isPinned: true,
+    },
+  ];
+
+  const otherProjects = [
+    {
+      name: "For Blades & Sorcery",
+      desc: "Medieval fantasy game powered by Unity consisting of 3 levels and a player fighting various enemies. Within a 5-person team, I implemented the enemy AI logic",
+      skills: ["C#", "Unity", "Trello", "CI", "Game Development"],
+      isAcademic: true,
+    },
+    {
+      name: "rgb-dws",
+      href: "https://os.mbed.com/users/kemzebra/code/rgb-data-with-stats/",
+      desc: "Statistical analysis of live RGB data gathered from the interactions between a Nucleo-64 board, a TCS3200 color sensor, and with mbed API",
+      skills: ["C++", "Embedded Systems"],
+      isAcademic: true,
+    },
+  ];
+
+  return (
+    <main className="projects-container">
+      <h1>Featured</h1>
+      <ProjectCards projects={featuredProjects} />
+      <h1>Open Source / Academic Projects</h1>
+      <ProjectCards projects={otherProjects} />
+    </main>
+  );
 }
 
 export default Projects;
